a52caf81574e200a24a979433224ce5eaa926cfa
Alexandra Klochko <alexandra@bunch.ai>
2017-12-08 14:30:14 +0300

    one player can play the game.

--- python/test_game.py
+++ python/test_game.py
@@ -3,6 +3,7 @@
 class Game():
     rows = 3
     colums = 3
+    state = []
     
     def frame(self):
         board =  ""
@@ -15,16 +16,34 @@ class Game():
                 board += "\n|/|\| \n"
 
             for j in range(0,self.colums):
+                thing = "o"
+
+                for piece in self.state:   
+                    if piece[0]==i and piece[1]==j:
+                        thing = 'x'
+
                 if j == 0:
-                    board += "o"
+                    board += thing
                 else:
-                    board += "-o"    
+                    board += "-" + thing
 
         return board
 
-
+    def place(self,x,y):
+        piece = (x,y)
+        self.state.append(piece)
         
 if __name__ == "__main__":
     game=Game()
     board = game.frame()
     print(board)
+
+    for op in range(0,3):
+
+        text_white = input("White player turn:")
+
+        x= int(text_white[0])-1
+        y= int(text_white[2])-1
+        game.place(x,y)
+
+        print(game.frame())
