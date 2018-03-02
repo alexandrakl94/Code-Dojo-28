748b838e5c0fa752a411ddbf330cddae0ae07383
Alexandra Klochko <alexandra@bunch.ai>
2018-03-02 17:00:11 +0100

    mocked imput

--- python/test_game.py
+++ python/test_game.py
@@ -1,7 +1,8 @@
 "A test suite for a Three Men's Morris game"
-import game
 from game import Game
 import mock
+
+import unittest
   
 
 def test_new_game_starts_with_valid_default_values():
@@ -116,17 +117,24 @@ def test_has_piece_left_should_return_false_when_white_count_equals_0():
   assert hasPiece == False
 
 
-# TODO: test the user input
+# TODO: change code to make sure we can exit a loop
+# 1. ask user if he she wants to exit
+ 
+class TestNotMockedFunction(unittest.TestCase):
+    
+    @mock.patch('builtins.input', return_value="1 1")
+    def test_start_should_read_user_input(self, input_mock):
+          
+        # arrange
+        my_game = Game()
 
-def test_start_should_read_user_input():
-      
-  # arrange
-  my_game = Game()
+        # act
+        my_game.start()
+
+        # assert
+        input_mock.assert_called()
+
+
+if __name__ == '__main__':
+    unittest.main()
   
-  # act
-  with mock.patch.object(my_game, 'start',
-                        wraps=my_game.start) as monkey:
-         my_game.start()
-         monkey.assert_called()
-
-      
